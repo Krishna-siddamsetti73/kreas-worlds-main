c8061df50318827660e3dbd134d58cca42d418ba
Krishna-siddamsetti73 <shivrikshna73@gmail.com>
2026-05-11 17:24:51 +0530

    hjj

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
